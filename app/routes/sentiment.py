3f4b23a72dd45da4cb74ac7d11b15b073731e07b
Suroz7 <srz.grz@gmail.com>
2025-02-14 11:11:25 +0200

    refined sentiment.py to acces hugginface api

--- app/routes/sentiment.py
+++ app/routes/sentiment.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
-from app.services.sentiment import analyze_sentiment
+from transformers import pipeline
 
 router = APIRouter()
 
+# Use Hugging Face API instead of requiring local PyTorch/TensorFlow
+sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english", device=-1)
+
 @router.post("/analyze")
 async def analyze(text: str):
-    sentiment = analyze_sentiment(text)
+    sentiment = sentiment_pipeline(text)
     return sentiment
